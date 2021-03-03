4ce7f989f8a44d6fdfd6237016df52772f21d97c
SirYavuz <51060721+SirYavuz@users.noreply.github.com>
2021-03-03 21:48:39 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,7 @@
 SYConfig = {}
 
+SYConfig.DisardanCek = true
+SYConfig.IcerdenCek = true
+SYConfig.TusKullan = true
+SYConfig.Tus = 38
+SYConfig.TusDurumu = true -- false aracın içindeyken tuşu aktif eder. / true aracın dışından tuş ile çekmeyi aktif eder. 
