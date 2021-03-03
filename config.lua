1d1246551fc77a488e422f73496dc8ab6591f4c9
SirYavuz <51060721+SirYavuz@users.noreply.github.com>
2021-03-03 21:50:47 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 SYConfig = {}
 
-SYConfig.DisardanCek = true
-SYConfig.IcerdenCek = true
+SYConfig.DisardanCek = true -- Aracı dışarıdan çekme komutunu "true" ile aktif, "false" ile pasif duruma getirebilirsiniz.
+SYConfig.IcerdenCek = true -- Aracı içindeyken çekme komutunu "true" ile aktif, "false" ile pasif duruma getirebilirsiniz.
 SYConfig.TusKullan = false -- Tuş kullanmak isteyenler true yapabilir fakat tuş kullanımı ms değerine olumsuz etki etmektedir.
 SYConfig.Tus = 38
 SYConfig.TusDurumu = true -- false aracın içindeyken tuşu aktif eder. / true aracın dışından tuş ile çekmeyi aktif eder. 
