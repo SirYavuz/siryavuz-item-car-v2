f3fe8b9ff9864fbe310e87fd7881092908aa7f4e
SirYavuz <51060721+SirYavuz@users.noreply.github.com>
2021-03-03 21:49:47 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@ SYConfig = {}
 
 SYConfig.DisardanCek = true
 SYConfig.IcerdenCek = true
-SYConfig.TusKullan = true
+SYConfig.TusKullan = false -- Tuş kullanmak isteyenler true yapabilir fakat tuş kullanımı ms değerine olumsuz etki etmektedir.
 SYConfig.Tus = 38
 SYConfig.TusDurumu = true -- false aracın içindeyken tuşu aktif eder. / true aracın dışından tuş ile çekmeyi aktif eder. 
